c8b967679e2e1c7af40438339fa3a85e008e3ed5
NickWilson01 <78823835+NickWilson01@users.noreply.github.com>
2025-01-15 11:30:27 -0500

    Add project files.

---
+++ HaverGroupProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HaverGroupProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
