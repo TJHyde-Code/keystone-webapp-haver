875a4c6d4888a3d6e0122ee77c7f7a58e2a5decb
TJHyde-Code <thyde1@ncstudents.niagaracollege.ca>
2025-01-16 21:52:10 -0500

    Db created, connect string created. Initializer skeleton created. Common NuGet File created.

---
+++ HaverGroupProject/Data/HaverInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HaverGroupProject.Models;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace HaverGroupProject.Data
+{
+    public static class HaverInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider,
+            bool DeleteDatabase = false, bool UseMigrations = true, bool SeedSampleData = true)
+        {
+            using (var context = new HaverContext(
+                serviceProvider.GetRequiredService<DbContextOptions<HaverContext>>()))
+            {
+                #region Database Prep
+                try
+                {
+                    
+                  
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.GetBaseException().Message);
+                }
+                #endregion
+
+
+                #region Seed Required Data
+
+
+                #endregion
+            }//Using Close
+
+        }//Initialize close
+
+    }//HaverInitializer Class close.
+}//Namespace close
